3d15be77952805fe9b70f3fcfacdee46f814819f
sukantamajhi <sukantamajhi799@gmail.com>
2023-11-26 03:05:02 +0530

    Errors fixed

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-const express = require("express")
-const morgan = require("morgan")
-require("dotenv").config()
-const connectToDb = require("./src/database/db")
-const router = require("./src/routes")
-const cors = require("cors")
-const config = require("./src/config")
-const swaggerUi = require('swagger-ui-express')
-const swaggerFile = require('./swagger_output.json')
-const logger = require("./logger/logger")
+const express = require("express");
+const morgan = require("morgan");
+require("dotenv").config();
+const connectToDb = require("./src/database/db");
+const router = require("./src/routes");
+const cors = require("cors");
+const config = require("./src/config");
+const logger = require("./logger/logger");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 if (config.nodeEnv !== "production") {
-    app.use(morgan("tiny"))
+	app.use(morgan("tiny"));
 }
 
-connectToDb()
+connectToDb();
 
-app.use("/api", router)
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use("/api", router);
 
 app.get("/", (req, res) => {
-    res.json("Hello world")
-})
+	res.json("Hello world");
+});
 
 app.listen(4000, () => {
-    logger.info("⚡️ Server is running on port 4000 ⚡️")
-})
+	logger.info("⚡️ Server is running on port 4000 ⚡️");
+});
